b62e4618aa610e60f674f3f9b29963e7a39b8c85
Hardik <hardik.singh.hs2212@gmail.com>
2026-01-06 15:45:22 +0530

    feat(pages): added total unpaid feature

--- src/constants/Icons.tsx
+++ src/constants/Icons.tsx
@@ -71,3 +71,42 @@ export const IconTrash: React.FC<IconProps> = (props) => (
     <line x1="14" y1="11" x2="14" y2="17"></line>
   </svg>
 );
+
+export const IconFilter: React.FC<IconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg"
+    fill="none" viewBox="0 0 24 24"
+    strokeWidth={1.5} stroke="currentColor"
+    className="w-6 h-6" {...props}><path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.572a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
+    /></svg>
+);
+
+export const IconSearch: React.FC<IconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+    viewBox="0 0 24 24" strokeWidth={1.5}
+    stroke="currentColor" className="w-6 h-6 " {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round"
+      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5
+                 7.5 0 0010.607 10.607z" /></svg>
+);
+
+export const IconDownload: React.FC<IconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg"
+    width="18" height="18" viewBox="0 0 24 24"
+    fill="none" stroke="currentColor" strokeWidth="2"
+    strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line>
+  </svg>
+);
+
+export const IconPrint: React.FC<IconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg"
+    width="18" height="18" viewBox="0 0 24 24"
+    fill="none" stroke="currentColor" strokeWidth="2"
+    strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <polyline points="6 9 6 2 18 2 18 9"></polyline>
+    <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 
+    2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"></path><rect x="6"
+      y="14" width="12" height="8"></rect></svg>
+);
